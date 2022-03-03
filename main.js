918d3f17c4385a3cc7b2d9beacca337d649cc57d
Mikuo <2992868880@qq.com>
2022-03-03 21:26:02 +0800

    完成了home首页开发

--- main.js
+++ main.js
@@ -3,6 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装弹窗的方法
+uni.$showMsg = function(title = '数据加载失败!', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
