65aa562e7aa22521d72ca7f53bfb3116527425f4
Mikuo <2992868880@qq.com>
2022-03-05 21:58:51 +0800

    完成了商品列表页面的开发

--- unpackage/dist/dev/mp-weixin/common/runtime.js
+++ unpackage/dist/dev/mp-weixin/common/runtime.js
@@ -105,11 +105,11 @@
 /******/
 /******/
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"components/my-search/my-search":1,"components/uni-icons/uni-icons":1,"components/uni-search-bar/uni-search-bar":1,"components/uni-tag/uni-tag":1};
+/******/ 		var cssChunks = {"components/my-search/my-search":1,"components/my-goods/my-goods":1,"components/uni-icons/uni-icons":1,"components/uni-search-bar/uni-search-bar":1,"components/uni-tag/uni-tag":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
-/******/ 				var href = "" + ({"components/my-search/my-search":"components/my-search/my-search","components/uni-icons/uni-icons":"components/uni-icons/uni-icons","components/uni-search-bar/uni-search-bar":"components/uni-search-bar/uni-search-bar","components/uni-tag/uni-tag":"components/uni-tag/uni-tag"}[chunkId]||chunkId) + ".wxss";
+/******/ 				var href = "" + ({"components/my-search/my-search":"components/my-search/my-search","components/my-goods/my-goods":"components/my-goods/my-goods","components/uni-icons/uni-icons":"components/uni-icons/uni-icons","components/uni-search-bar/uni-search-bar":"components/uni-search-bar/uni-search-bar","components/uni-tag/uni-tag":"components/uni-tag/uni-tag"}[chunkId]||chunkId) + ".wxss";
 /******/ 				var fullhref = __webpack_require__.p + href;
 /******/ 				var existingLinkTags = document.getElementsByTagName("link");
 /******/ 				for(var i = 0; i < existingLinkTags.length; i++) {
